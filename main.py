ad90beea7fd3a435559f2646267097db8738dd17
jadmonino <jadmonino@gmail.com>
2025-08-07 13:51:26 +0200

    Actualizar pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+distancia1 = 0
+
 def on_forever():
+    # global distancia1
     MiniCar.led_rgb(LED_rgb_L_R.LED_L, LED_color.GREEN1)
     MiniCar.led_show()
     MiniCar.LED_OFF()
     MiniCar.PWM_LED_L(pwm_led_l.PWM_RED_R, 118)
     MiniCar.PWM_LED_R(pwm_led_r.PWM_GREEN_L, 118)
-    MiniCar.motor(Motorlist.M1, Direction1.FORWARD, 100)
-    MiniCar.motor(Motorlist.M2, Direction1.FORWARD, 100)
+    distancia1 = MiniCar.ultra()
+    basic.show_number(distancia1)
+    basic.pause(100)
+    if distancia1 < 30:
+        MiniCar.motor(Motorlist.M1, Direction1.FORWARD, 0)
+        MiniCar.motor(Motorlist.M2, Direction1.FORWARD, 0)
+    else:
+        MiniCar.motor(Motorlist.M1, Direction1.FORWARD, 60)
+        MiniCar.motor(Motorlist.M2, Direction1.FORWARD, 60)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,10 +1,18 @@
+let distancia1 = 0
 basic.forever(function () {
     MiniCar.led_rgb(LED_rgb_L_R.LED_L, LED_color.green1)
     MiniCar.led_show()
     MiniCar.LED_OFF()
     MiniCar.PWM_LED_L(pwm_led_l.pwm_red_r, 118)
     MiniCar.PWM_LED_R(pwm_led_r.pwm_green_l, 118)
-    MiniCar.motor(Motorlist.M1, Direction1.Forward, MiniCar.ultra())
-    MiniCar.motor(Motorlist.M2, Direction1.Backward, MiniCar.ultra())
-    MiniCar.setServo(45)
+    distancia1 = MiniCar.ultra()
+    basic.showNumber(distancia1)
+    basic.pause(100)
+    if (distancia1 < 30) {
+        MiniCar.motor(Motorlist.M1, Direction1.Forward, 0)
+        MiniCar.motor(Motorlist.M2, Direction1.Forward, 0)
+    } else {
+        MiniCar.motor(Motorlist.M1, Direction1.Forward, 60)
+        MiniCar.motor(Motorlist.M2, Direction1.Forward, 60)
+    }
 })
